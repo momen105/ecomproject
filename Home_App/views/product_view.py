ff33ee79388801f9ff2ecea174cad92575179634
Momen Tohin <63067387+momen105@users.noreply.github.com>
2022-04-07 00:50:41 +0600

    Update product_view.py

--- Home_App/views/product_view.py
+++ Home_App/views/product_view.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-
 # Import views
 from django.views.generic import ListView, DetailView
 from django.views import View
 # Models
 from Home_App.models.product_model import Product, Category
-
 # Mixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
